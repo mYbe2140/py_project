8d45ad30824c6d74ec1f12604414decf0b30264d
Aleksander Garmashov <alexandr.mybe@mail.ru>
2023-06-12 23:30:41 +0700

    add new func and add docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def line_up(text):
+    """
+    Переводит слово целиком в верхний регистр
+    """
     return text.upper()
+
+
+def first_letter_up(some_text):
+    """
+    Превращает первые буквы слов в строке в заглавные
+    """
+    return some_text.title()
